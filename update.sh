89d427d1c14dd5c8cbb4620bea79ac25540dfe74
Dmitriy Safronov <zimniy@cyberbrain.pw>
2024-01-14 13:10:37 +0400

    update (#4)
    Signed-off-by: Dmitriy Safronov <zimniy@cyberbrain.pw>

---
+++ update.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+git \
+  clone \
+  https://github.com/dmitriysafronov/ansible-project.git \
+  .update
+
+rsync -av \
+  --include=.github/workflows/ansible.yaml \
+  --include=collections/ansible_collections/README.md \
+  --include=collections/README.md \
+  --include=inventory/README.md \
+  --include=playbooks/README.md \
+  --include=roles/README.md\
+  --include=.gitignore \
+  --include=ansible.cfg \
+  --include=requirements.sh \
+  --include=update.sh \
+  --exclude=* \
+  --delete-after \
+  .update/ \
+  ./
+
+rm -rf .update/
